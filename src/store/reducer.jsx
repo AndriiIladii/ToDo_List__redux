d6c6c28b397e74489d75fb39a7e6c32c78947f10
AndriiIladii <iladiy21@gmail.com>
2024-06-18 15:39:32 +0300

    add todo bug

--- src/store/reducer.jsx
+++ src/store/reducer.jsx
@@ -5,8 +5,9 @@ const initialState = {
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TODO": {
-      const newState = state.concat(action.payload);
-      return newState;
+      return {
+        ...state,
+      };
     }
 
     // case "TOGGLE_TODO": {
